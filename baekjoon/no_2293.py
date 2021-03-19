c14e6357cb6231fd8678589f85510847cbe87f80
Jisun Hong <55406486+jisun1125@users.noreply.github.com>
2021-03-19 23:52:35 +0900

    Solve boj no.2293 problem

---
+++ baekjoon/no_2293.py
@@ -0,0 +1,13 @@
+# 2021-03-19
+# url : https://www.acmicpc.net/problem/2293
+# 2293 동전 1
+n, k = map(int, input().split())  # n: 동전의 종류, k: 가치의 합
+c = []
+for i in range(n):
+    c.append(int(input()))
+dp = [1] + [0 for _ in range(k)]
+for i in c:
+    for j in range(1, k+1):
+        if j - i >= 0:
+            dp[j] += dp[j-i]
+print(dp[k])
