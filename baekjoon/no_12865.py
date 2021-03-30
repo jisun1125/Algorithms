7080f00b80b03de8ae1f5490f2c3c03a923413bb
Jisun Hong <55406486+jisun1125@users.noreply.github.com>
2021-03-31 00:01:14 +0900

    Solve boj no.12865 problem

---
+++ baekjoon/no_12865.py
@@ -0,0 +1,19 @@
+# 2021-03-30
+# url : https://www.acmicpc.net/problem/12865
+# 12865 평범한 배낭
+n, k = map(int, input().split())  # n: 물품의 수, k: 가방에 넣을 수 있는 최대 무게
+dp = [[0] * (k+1) for _ in range(n+1)]
+m = [[0, 0]]
+for i in range(n):
+    w, v = map(int, input().split())  # w: 각 물건의 무게, v: 물건의 가치
+    m.append([w, v])
+
+for i in range(1, n+1):
+    w = m[i][0]
+    v = m[i][1]
+    for j in range(1, k+1):
+        if j < w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j-w] + v, dp[i-1][j])
+print(dp[n][k])
